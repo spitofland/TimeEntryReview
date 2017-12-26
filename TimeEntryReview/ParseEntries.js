0c6a7098a5dcb7cab025b1c1d202f1f22e4a2062
Kyle <24555524+spitofland@users.noreply.github.com>
2017-12-26 14:13:10 -0700

    Add Comparisons with Processed Entries
    Also, updated code formatting.

--- TimeEntryReview/ParseEntries.js
+++ TimeEntryReview/ParseEntries.js
@@ -1,27 +1,103 @@
-function ParseEntries(textDump)
-{
-  // Get each line and call ParseEntry on that line
-  var lines = textDump.split("\n");
-  //var lines = textDump.match(/[^\n]*/g);  // Regex form
-  
-  return lines.reduce( function (entries, line) {
-    var entry = ParseEntry(line);
-    if( entry != undefined )
-    {
+function ParseEntries( textDump ) {
+  // This uses a string delimiter, an equivalent RegEx would be /[^\n]*/g
+  return ParseLines("\n",textDump,ParseEntry);
+}
+
+function ParseProcessedEntries( textDump ) {
+  // This uses a string delimiter, an equivalent RegEx would be /[^\n]*/g
+  return ParseLines("\n",textDump,ParseProcessedEntry);
+}
+
+// ParseLines can take a string delimiter or a RegEx for 'splitter'
+function ParseLines( splitter, textDump, Parser ) {
+  var lines;
+  if( splitter instanceof RegExp ) {
+    lines = textDump.match(splitter);
+  }
+  else {
+    lines = textDump.split(splitter);
+  }
+
+  return lines.reduce( function ( entries, line ) {
+    var entry = Parser(line);
+    if( entry != undefined ) {
       entries.push(entry);
     }
     return entries;
   }, [] );
 }
 
-function ParseEntry(line)
-{
+function ParseEntry( line ) {
   //Match Something Like: "LH Dec 25, 2017 08:00 Dec 25, 2017 17:00 8.00 Automatic Holiday"
-  var match = /(\w+)\s+([^:]+:\d+)\s+([^:]+:\d+)\s+(\d+\.\d*)\s+(.+)$/.exec(line);
-  if( match == undefined )
-  {
+  var regEx = /(\w+)\s+([^:]+:\d+(\s+[AP]M)?)\s+([^:]+:\d+(\s+[AP]M)?)\s+(\d+\.?\d*)\s+(.*)$/;
+  var match = regEx.exec(line);
+  if( match == undefined ) {
     return undefined;
   }
   return {entryType: match[1], startDateTime: new Date(Date.parse(match[2])),
-    endDateTime: new Date(Date.parse(match[3])), duration: match[4], other: match[5]};
+    endDateTime: new Date(Date.parse(match[4])), duration: match[6], other: match[7]};
+}
+
+function ParseProcessedEntry( line ) {
+  //Match Something Like: "10/17  TUE  00000  0  1375  RG  0200  0000  0000  0000  0000"
+  //                  Or: "10/18  WED  00000  0  1500  CD  0000  0000  0000  0000  0100"
+  //                      |   Date   |          |Time|Type|         Duration           |
+  var regEx = /0?(\d+)\/(\d+)\s+\w+\s+\w+\s+\w+\s+(\d+)(\d\d)\s+(\w+)[^1-9]+([1-9]\d*)(\D|$)/;
+  var match = regEx.exec(line);
+  if( match == undefined ) {
+    return undefined;
+  }
+
+  var now = new Date();
+  var entryYear = now.getFullYear();
+  if( match[1] > now.getMonth()+1 ) {
+    entryYear -= 1;
+  }
+  var dateTimeStr = match[1]+"\/"+match[2]+"\/"+entryYear + " " + match[3]+":"+(match[4]*0.6);
+
+  return {entryType: match[5], startDateTime: new Date(Date.parse(dateTimeStr)),
+    endDateTime: undefined, duration: (match[6]/100), other: ""};
+}
+
+function CompareEntryLists( entries, processedEntries ) {
+  var entryIndex = 0;
+  var differences = {entries: [], processedEntries: []};
+
+  // Add non-matching entries to 'differences'
+  for( var procIndex = 0; procIndex < processedEntries.length; procIndex++ ) {
+    var procEntry = processedEntries[procIndex];
+    var unmatchedDuration = procEntry.duration;
+    var procEntryAdded = false;
+
+    for( entryIndex; entryIndex < entries.length &&
+          entries[entryIndex].startDateTime < procEntry.startDateTime; entryIndex++ ) {
+      differences.entries.push(entries[entryIndex]);
+    }
+
+    // Multiple consecutive 'entries' may contribute to the same 'processed entry'
+    for( var lastEndTime = procEntry.startDateTime;
+          (entryIndex < entries.length) && (entries[entryIndex].duration <= unmatchedDuration) &&
+          (entries[entryIndex].startDateTime.getTime() == lastEndTime.getTime()); entryIndex++ ) {
+      lastEndTime = entries[entryIndex].endDateTime;
+      unmatchedDuration -= entries[entryIndex].duration;
+      if( entries[entryIndex].entryType != procEntry.entryType ) {
+        differences.entries.push(entries[entryIndex]);
+        if( !procEntryAdded ) {
+          procEntryAdded = true;
+          differences.processedEntries.push(procEntry);
+        }
+      }
+    }
+    procEntry.endDateTime = AddTime(lastEndTime, 0, 60*unmatchedDuration);  // Best guess end time
+
+    if( unmatchedDuration > 0 && !procEntryAdded ) {
+      differences.processedEntries.push(procEntry);
+    }
+  }
+
+  for( entryIndex; entryIndex < entries.length; entryIndex++ ) {
+    differences.entries.push(entries[entryIndex]);
+  }
+
+  return differences;
 }
--- TimeEntryReview/TimeEntryReview.js
+++ TimeEntryReview/TimeEntryReview.js
@@ -2,50 +2,52 @@ var timeLabelSpan = 4;
 
 // NOTE: This code does not handle entries that cross midnight
 
-function AddTime(dateObj, hours, minutes)
-{
+function AddTime(dateObj, hours, minutes) {
   var result = new Date(dateObj);
   result.setHours(result.getHours()+hours);
   result.setMinutes(result.getMinutes()+minutes);
   return result;
 }
 
-function DiffMinutesIgnoringDate(startTime, endTime)
-{
+function DiffMinutesIgnoringDate(startTime, endTime) {
   return 60*(endTime.getHours()-startTime.getHours()) + endTime.getMinutes()-startTime.getMinutes();
 }
 
-function ShortTimeFormat(dateObj)
-{
+function NumOfTimeSlices(startTime, endTime) {
+  return Math.ceil(DiffMinutesIgnoringDate(startTime,endTime)/15);
+}
+
+function ShortTimeFormat(dateObj) {
   return /^\d+:\d+/.exec(dateObj.toTimeString());
 }
 
-function FindEarliestStartTime(entries)
-{
-  return entries.reduce( function (startTime, entry) {
-    if( DiffMinutesIgnoringDate(entry.startDateTime,startTime) >= 0 )
-    {
+function FindEarliestStartTime(entries, differences) {
+  var MinStart = function (startTime, entry) {
+    if( DiffMinutesIgnoringDate(entry.startDateTime,startTime) >= 0 ) {
       startTime = entry.startDateTime;
     }
     return startTime;
-  }, new Date(2020,0,1,23,45) );  // Initial value
+  };
+  var prevStartTime = entries.reduce( MinStart, new Date(2020,0,1,23,45) );
+  prevStartTime = differences.entries.reduce( MinStart, prevStartTime );
+  return differences.processedEntries.reduce( MinStart, prevStartTime );
 }
 
-function FindLatestEndTime(entries)
-{
-  return entries.reduce( function (endTime, entry) {
+function FindLatestEndTime(entries, differences) {
+  var MaxEnd = function (endTime, entry) {
     if( (DiffMinutesIgnoringDate(entry.endDateTime,endTime) <= 0 &&
-        (endTime.getHours != 0 || endTime.getMinutes() != 0)) ||
-      (entry.endDateTime.getHours() == 0 && entry.endDateTime.getMinutes() == 0) )
-    {
+          (endTime.getHours != 0 || endTime.getMinutes() != 0)) ||
+          (entry.endDateTime.getHours() == 0 && entry.endDateTime.getMinutes() == 0) ) {
       endTime = entry.endDateTime;
     }
     return endTime;
-  }, new Date(2020,0,1,0,15) );  // Initial value
+  };
+  var prevEndTime = entries.reduce( MaxEnd, new Date(2020,0,1,0,15) );
+  prevEndTime = differences.entries.reduce( MaxEnd, prevEndTime );
+  return differences.processedEntries.reduce( MaxEnd, prevEndTime );
 }
 
-function RoundDownToHour(dateObj)
-{
+function RoundDownToHour(dateObj) {
   var rounded = new Date(dateObj);
   rounded.setMinutes(0);
   rounded.setSeconds(0);
@@ -53,43 +55,36 @@ function RoundDownToHour(dateObj)
   return rounded;
 }
 
-function RoundUpToHour(dateObj)
-{
+function RoundUpToHour(dateObj) {
   var rounded = RoundDownToHour(dateObj);
-  if( rounded < dateObj )
-  {
+  if( rounded < dateObj ) {
     rounded.setHours(rounded.getHours()+1);
   }
   return rounded;
 }
 
-function RoundDownToDate(dateObj)
-{
+function RoundDownToDate(dateObj) {
   var rounded = RoundDownToHour(dateObj);
   rounded.setHours(0);
   return rounded;
 }
 
-function FillGridWithEmptyCells(numSlices)
-{
+function FillGridWithEmptyCells(numSlices) {
   var str = "";
-  for( var j=0; j<8; j++ )
-  {
-    for( var i=0; i<numSlices; i++ )
-    {
-      str += "<div class='empty' style='grid-column:" + (j+1) + "/span 1; grid-row:" + (i+2) + "/span 1'></div>";
+  for( var j=0; j<8; j++ ) {
+    for( var i=0; i<numSlices; i++ ) {
+      str += "<div class='empty' style='grid-column:" + (j+1) + "/span 1; grid-row:" +
+        (i+2) + "/span 1'></div>";
     }
   }
   return str;
 }
 
-function DrawDayOfWeekLabels(entry)
-{
+function DrawDayOfWeekLabels(entry) {
   var str = "";
   var day = new Date(entry.startDateTime);
   day.setDate(day.getDate()-day.getDay());
-  for(var i=0; i<7; day.setDate(day.getDate()+1), i++ )
-  {
+  for(var i=0; i<7; day.setDate(day.getDate()+1), i++ ) {
     str += "<div class='DayOfWeekLabel' style='grid-column: " + (i+2) + "/span 1;'>" +
       /^\w+\s\w+\s\d+/.exec(day.toDateString()) + "</div>";
   }
@@ -97,50 +92,59 @@ function DrawDayOfWeekLabels(entry)
   return str;
 }
 
-function DrawTimeList(startTime, numSlices)
-{
+function DrawTimeList(startTime, numSlices) {
   var str = "";
   for( var time=new Date(startTime), i = 0; i < numSlices;
-    time = AddTime(time,0,15*timeLabelSpan), i += timeLabelSpan )
-  {
+        time = AddTime(time,0,15*timeLabelSpan), i += timeLabelSpan ) {
     str += "<div class='timeLabel' style='grid-row: " + (i+2) + "/span " +
       timeLabelSpan + "'><span>" + ShortTimeFormat(time) + "</span></div>"
   }
   return str;
 }
 
-function DrawEntries(entries, startTime)
-{
-  var str = "";
-  for( var e in entries )
-  {
-    var dayOfWeek = entries[e].startDateTime.getDay();
-    var startTimeSlice = Math.ceil(DiffMinutesIgnoringDate(startTime,entries[e].startDateTime)/15);
-    var endTimeSlice = Math.ceil(DiffMinutesIgnoringDate(startTime,entries[e].endDateTime)/15);
-    if( endTimeSlice <= 0 )   // Handle Midnight
-    {
-      endTimeSlice += 96;
-    }
+function GetEntryColRowStr(entry, startTime) {
+  var endTimeSlice = NumOfTimeSlices(startTime,entry.endDateTime);
+  if( endTimeSlice <= 0 ) {   // Handle Midnight
+    endTimeSlice += 96;
+  }
+  return "style='grid-column: " + (entry.startDateTime.getDay()+2) + "/span 1; grid-row: " +
+    (NumOfTimeSlices(startTime,entry.startDateTime)+2) + "/" + (endTimeSlice + 2) + "'";
+}
 
-    str += "<div class='" + entries[e].entryType + "' style='grid-column: " + (dayOfWeek+2) +
-      "/span 1; grid-row: " + (startTimeSlice+2) + "/" + (endTimeSlice+2) + "'>" +
-      entries[e].entryType + " : " + entries[e].duration + " Hr" +
-      (entries[e].duration > 1 ? "s" : "") + "<br/>" +
-      ShortTimeFormat(entries[e].startDateTime) + " - " +
-      ShortTimeFormat(entries[e].endDateTime) + "<br/>" +
-      entries[e].other + "</div>";
+function GetEntryDescription(entry) {
+  return entry.entryType + " : " + entry.duration + " Hr" + (entry.duration > 1 ? "s" : "") +
+    "<br/>" + ShortTimeFormat(entry.startDateTime) + " - " + ShortTimeFormat(entry.endDateTime) +
+    "<br/>" + entry.other;
+}
+
+function DrawEntries(entries, startTime) {
+  var str = "";
+  for( var e in entries ) {
+    str += "<div class='" + entries[e].entryType + "' " + GetEntryColRowStr(entries[e],startTime) +
+      ">" + GetEntryDescription(entries[e]) + "</div>";
   }
   return str;
 }
 
-function DrawWeek(entries)
-{
+function DrawDifferences(differences, startTime) {
+  var str = "";
+  for( var e of differences.entries ) {
+    str += "<div class='entryDiff' " + GetEntryColRowStr(e,startTime) +
+      ">Unmatched Entry<br/>" + GetEntryDescription(e) + "</div>";
+  }
+  for( var p of differences.processedEntries ) {
+    str += "<div class='processedDiff' " + GetEntryColRowStr(p,startTime) +
+      ">Unmatched Processed Entry<br/>" + GetEntryDescription(p) + "</div>";
+  }
+  return str;
+}
+
+function DrawWeek(entries,differences) {
   var str = "<div class='week'>";
-  var startTime = RoundDownToHour(FindEarliestStartTime(entries));
-  var endTime = RoundUpToHour(FindLatestEndTime(entries));
-  var numSlices = Math.ceil(DiffMinutesIgnoringDate(startTime,endTime)/15);
-  if( numSlices <= 0 )   // Handle Midnight
-  {
+  var startTime = RoundDownToHour(FindEarliestStartTime(entries,differences));
+  var endTime = RoundUpToHour(FindLatestEndTime(entries,differences));
+  var numSlices = NumOfTimeSlices(startTime,endTime);
+  if( numSlices <= 0 ) {   // Handle Midnight
     numSlices += 96;
   }
 
@@ -148,13 +152,13 @@ function DrawWeek(entries)
   str += DrawTimeList(startTime,numSlices);
   str += DrawDayOfWeekLabels(entries[0]);
   str += DrawEntries(entries,startTime);
+  str += DrawDifferences(differences,startTime);
 
   str += "</div>";
   document.getElementById("Planner").innerHTML += str;
 }
 
-function GetEntriesForWeek(entries,weekDate)
-{
+function GetEntriesForWeek(entries,weekDate) {
   var weekStartDate = new Date(weekDate);
   weekStartDate.setHours(0);
   weekStartDate.setDate(weekStartDate.getDate()-weekStartDate.getDay());
@@ -166,8 +170,7 @@ function GetEntriesForWeek(entries,weekDate)
   });
 }
 
-function DrawPlanner(entries)
-{
+function DrawPlanner(entries,differences) {
   document.getElementById("Planner").innerHTML = "";
   entries.sort( function (entry1,entry2) { return entry1.startDateTime - entry2.startDateTime; });
   var startDate = RoundDownToDate(entries[0].startDateTime);
@@ -177,16 +180,36 @@ function DrawPlanner(entries)
   var weekEnd = new Date(date);
   weekEnd.setDate(weekEnd.getDate()+7);
 
-  for( ; date < endDate; date.setDate(date.getDate()+7), weekEnd.setDate(weekEnd.getDate()+7) )
-  {
-    DrawWeek( entries.filter( function (entry) {
+  for( ; date < endDate; date.setDate(date.getDate()+7), weekEnd.setDate(weekEnd.getDate()+7) ) {
+    var IsInWeek = function (entry) {
       return (entry.startDateTime > date) && (entry.startDateTime < weekEnd);
-    }) );
+    };
+    var weekDiff = {entries: differences.entries.filter(IsInWeek),
+      processedEntries: differences.processedEntries.filter(IsInWeek)};
+    DrawWeek( entries.filter(IsInWeek), weekDiff );
   }
 }
 
-function ParseEntryTable()
-{
+function ParseEntryTables() {
   var entries = ParseEntries(document.getElementById("EntryText").value);
-  DrawPlanner(entries);
+  var differences = {entries: [], processedEntries: []};
+  if( !document.getElementById("ProcessedEntryTextDiv").classList.contains("hidden") ) {
+    var procEntries = ParseProcessedEntries(document.getElementById("ProcessedEntryText").value);
+    differences = CompareEntryLists(entries,procEntries);
+  }
+  DrawPlanner(entries,differences);
+}
+
+function ToggleProcessedTable() {
+  var processedTableClasses = document.getElementById("ProcessedEntryTextDiv").classList;
+  var drawProcessedButton = document.getElementById("HideProcessed");
+  if( processedTableClasses.contains("hidden") ) {
+    processedTableClasses.remove("hidden");
+    drawProcessedButton.innerHTML = "Hide Processed Entries Table";
+  }
+  else {
+    processedTableClasses.add("hidden");
+    drawProcessedButton.innerHTML = "Compare With Processed Entries";
+  }
+  ParseEntryTables();
 }
